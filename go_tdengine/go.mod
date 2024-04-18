d1accbeb86b35d0decfcc4f6f9ae6ff8e1b91f88
Eikoo <114289969+whxxxxxxxxxx@users.noreply.github.com>
2024-04-18 14:22:37 +0800

    init

---
+++ go_tdengine/go.mod
@@ -0,0 +1,10 @@
+module go_tdengine
+
+go 1.22.0
+
+require github.com/taosdata/driver-go/v3 v3.5.3
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+)
---
+++ go_tdengine/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/taosdata/driver-go/v3/taosSql"
+)
+
+func main() {
+	var taosUri = "root:taosdata@tcp(localhost:6030)/"
+	taos, err := sql.Open("taosSql", taosUri)
+	if err != nil {
+		fmt.Println("failed to connect TDengine, err:", err)
+		return
+	}
+}
